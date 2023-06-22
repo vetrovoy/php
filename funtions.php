95825b00e6df2b6498ba96cab47201195d097b44
vetrovoy <v3trovoy@gmail.com>
2023-06-22 14:11:51 +0300

    recaptcha

--- funtions.php
+++ funtions.php
@@ -1,3 +1,4 @@
+<?php 
 function message($template, $post, $order)
 {
     $message = $template;
@@ -31,3 +32,41 @@ function validatePhone($phoneS)
 {
     return preg_match("/^\+?[0-9\(\)\-\s]{6,}$/", $phoneS);
 }
+
+function verify_recaptcha($response_token)
+{
+    // Определяем переменные, которые будут использоваться в запросе
+    $secret = 'SECRET_CODE';
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+
+    // Создаем словарь с данными, которые будут отправлены на сервер reCAPTCHA для проверки
+    $data = array(
+        'secret' => $secret,
+        'response' => $response_token
+    );
+
+    // Отправляем POST-запрос на сервер reCAPTCHA и получаем ответ в формате JSON
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === false) {
+        return false;
+    }
+
+    // Парсим ответ, полученный от сервера reCAPTCHA
+    $json_response = json_decode($result, true);
+
+    // Проверяем, прошла ли проверка reCAPTCHA
+    if ($json_response['success']) {
+        return true;
+    } else {
+        return false;
+    }
+}
+?>
